f44556681ffbca92959ff9890d9ee9231d9697be
geniuxy <2238384910@qq.com>
2026-02-27 12:01:09 +0800

    first commit Enemy's Rotation

---
+++ Assets/Enemy.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    [SerializeField] private float turnSpeed = 10.0f;
+    [SerializeField] private Transform[] wayPoints;
+    private int wayPointIndex;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+    }
+
+    private void Update()
+    {
+        FaceTarget(agent.steeringTarget);
+        if (agent.remainingDistance < 0.5f)
+        {
+            agent.SetDestination(GetNextWayPoint());
+        }
+    }
+
+    private void FaceTarget(Vector3 newTarget)
+    {
+        Vector3 directionToTarget = newTarget - transform.position;
+        if (directionToTarget.magnitude == 0)
+        {
+            return;
+        }
+
+        directionToTarget.y = 0;
+
+        Quaternion newRotation = Quaternion.LookRotation(directionToTarget);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, turnSpeed * Time.deltaTime);
+    }
+
+    // Start is called before the first frame update
+    private Vector3 GetNextWayPoint()
+    {
+        if (wayPointIndex >= wayPoints.Length)
+        {
+            return transform.position;
+        }
+
+        Vector3 targetPoint = wayPoints[wayPointIndex++].position;
+
+        return targetPoint;
+    }
+}
