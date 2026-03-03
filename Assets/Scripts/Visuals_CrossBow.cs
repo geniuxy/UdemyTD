75aeb2a551051a6bac85dc55428715ee4f0a6ba9
geniuxy <2238384910@qq.com>
2026-03-03 14:27:04 +0800

    Dynamic Laser Strings Setup

--- Assets/Scripts/Visuals_CrossBow.cs
+++ Assets/Scripts/Visuals_CrossBow.cs
@@ -20,6 +20,26 @@ public class Visuals_CrossBow : MonoBehaviour
     [Space]
     [SerializeField] private Color startColor;
     [SerializeField] private Color endColor;
+
+    [Header("Front Glow String")] 
+    [SerializeField] private LineRenderer frontString_L;
+    [SerializeField] private LineRenderer frontString_R;
+
+    [Space]
+    [SerializeField] private Transform frontStartPoint_L;
+    [SerializeField] private Transform frontStartPoint_R;
+    [SerializeField] private Transform frontEndPoint_L;
+    [SerializeField] private Transform frontEndPoint_R;
+    
+    [Header("Back Glow String")] 
+    [SerializeField] private LineRenderer backString_L;
+    [SerializeField] private LineRenderer backString_R;
+
+    [Space]
+    [SerializeField] private Transform backStartPoint_L;
+    [SerializeField] private Transform backStartPoint_R;
+    [SerializeField] private Transform backEndPoint_L;
+    [SerializeField] private Transform backEndPoint_R;
     
     protected void Awake()
     {
@@ -35,6 +55,10 @@ public class Visuals_CrossBow : MonoBehaviour
     private void Update()
     {
         UpdateEmissionColor();
+        UpdateStringVisual(frontString_L, frontStartPoint_L, frontEndPoint_L);
+        UpdateStringVisual(frontString_R, frontStartPoint_R, frontEndPoint_R);
+        UpdateStringVisual(backString_L, backStartPoint_L, backEndPoint_L);
+        UpdateStringVisual(backString_R, backStartPoint_R, backEndPoint_R);
     }
 
     private void UpdateEmissionColor()
@@ -86,5 +110,11 @@ public class Visuals_CrossBow : MonoBehaviour
         }
 
         currentIntensity = maxIntensity;
+    }
+
+    private void UpdateStringVisual(LineRenderer lineRenderer, Transform startPoint, Transform endPoint)
+    {
+        lineRenderer.SetPosition(0, startPoint.position);
+        lineRenderer.SetPosition(1, endPoint.position);
     }
 }
